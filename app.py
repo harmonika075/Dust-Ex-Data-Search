ab18fd1de80e138ed8b1924016aa84a62f12111b
Dalnoki Mátyás <dalnoki@veproil.hu>
2025-04-07 23:00:00 +0200

    Fix: bind to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -36,5 +36,8 @@ def index():
     else:
         return render_template("index.html", results=[], results_to_show=[], show_more=False, query="", page=1)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
